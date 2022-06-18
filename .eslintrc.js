e082cfa32b9f91eb53eeb6177f6061b7ea33ad4b
Ilya Kazakou <ilya.kazakou@nbrown.co.uk>
2022-06-18 18:58:10 +0400

    FE-1: installed & configured eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "google",
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {},
+};
