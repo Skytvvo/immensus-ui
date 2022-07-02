a802cefc85d14b074ced17961bf97cb228d2bf49
Ilya Kazakou <ilya.kazakou@nbrown.co.uk>
2022-07-03 01:07:15 +0400

    FE-13: implemented logic

---
+++ app/stories/navigation/Menu/ProfileMenu/ProfileMenu.const.ts
@@ -0,0 +1,5 @@
+export const LC_PM = {
+  MY_PROFILE: 'My profile',
+  SETTINGS: 'Settings',
+  SIGN_OUT: 'Sign out',
+};
--- app/stories/navigation/Menu/ProfileMenu/ProfileMenu.tsx
+++ app/stories/navigation/Menu/ProfileMenu/ProfileMenu.tsx
@@ -1,11 +1,39 @@
 import type { FC } from 'react';
 import React from 'react';
+import { Avatar, Button, IconButton, Menu, MenuItem } from '@mui/material';
 
 const ProfileMenu: FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      <span>hello world</span>
-    </div>
+    <>
+      <IconButton onClick={handleClick}>
+        <Avatar />
+      </IconButton>
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClick={handleClose}
+        onClose={handleClose}
+      >
+        <MenuItem>
+          <Avatar /> My profile
+        </MenuItem>
+        <MenuItem>Settings</MenuItem>
+        <Button>Sign out</Button>
+      </Menu>
+    </>
   );
 };
 
