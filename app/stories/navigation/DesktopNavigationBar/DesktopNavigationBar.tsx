429df7d1f71f94edea9b4c1bc7acd2772a05097e
Ilya Kazakou <ilya.kazakou@nbrown.co.uk>
2022-07-02 19:04:05 +0400

    FE-11: added nav links into desktop nav bar, disabled no-shadow rule at eslint due to ts issue

--- app/stories/navigation/DesktopNavigationBar/DesktopNavigationBar.tsx
+++ app/stories/navigation/DesktopNavigationBar/DesktopNavigationBar.tsx
@@ -3,10 +3,13 @@ import type { FC } from 'react';
 import { AppBar, Button, Grid, Toolbar, Typography } from '@mui/material';
 import { Link } from '@remix-run/react';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ExploreRoundedIcon from '@mui/icons-material/ExploreRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
 import NavigationSearch from '../NavigationSearch/NavigationSearch';
+import DesktopNavigationLink from '../DesktopNavigatioLink';
+import { navigationRoutes } from '../../../globals/collections/navigation.enum';
 import { LC_DNB } from './DesktopNavigationBar.const';
-
-const homePageRoute = '#';
 
 const DesktopNavigationBar: FC = () => {
   return (
@@ -15,16 +18,27 @@ const DesktopNavigationBar: FC = () => {
         <Grid container>
           <Grid item xs={2}>
             <Typography variant="h6" align="center">
-              <Link to={homePageRoute}>{LC_DNB.logo}</Link>
+              <Link to={navigationRoutes.HOME}>{LC_DNB.logo}</Link>
             </Typography>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <NavigationSearch />
           </Grid>
           <Grid item xs={3}>
             <Button startIcon={<AddCircleIcon />} variant="contained">
               {LC_DNB.createNewPostButton}
             </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <DesktopNavigationLink route={navigationRoutes.HOME}>
+              <HomeRoundedIcon />
+            </DesktopNavigationLink>
+            <DesktopNavigationLink route={navigationRoutes.EXPLORE}>
+              <ExploreRoundedIcon />
+            </DesktopNavigationLink>
+            <DesktopNavigationLink route={navigationRoutes.DIRECT}>
+              <SendRoundedIcon />
+            </DesktopNavigationLink>
           </Grid>
         </Grid>
       </Toolbar>
