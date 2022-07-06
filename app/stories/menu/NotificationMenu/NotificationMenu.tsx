dd44234221c02a3e6cb5f2249932cb7db088c7fc
Ilya Kazakou <ilya.kazakou@nbrown.co.uk>
2022-07-06 23:31:40 +0400

    FE-14: configured basic notification menu

--- app/stories/menu/NotificationMenu/NotificationMenu.tsx
+++ app/stories/menu/NotificationMenu/NotificationMenu.tsx
@@ -1,8 +1,26 @@
 import type { FC } from 'react';
 import React from 'react';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import { Divider, MenuItem, Stack, Typography } from '@mui/material';
+import MenuWrapper from '../../wrappers/MenuWrapper';
+import { notificationMenuMockedNotifications } from './NotificationMenu.const';
 
 const NotificationMenu: FC = () => {
-  return <div />;
+  return (
+    <MenuWrapper menuIcon={<NotificationsRoundedIcon />}>
+      {notificationMenuMockedNotifications.map(({ label, message }) => (
+        <MenuItem key={label}>
+          <Stack spacing={1}>
+            <Typography variant="h6" align="left">
+              {label}
+            </Typography>
+            <Typography align="justify">{message}</Typography>
+            <Divider />
+          </Stack>
+        </MenuItem>
+      ))}
+    </MenuWrapper>
+  );
 };
 
 export default NotificationMenu;
